519af23622731b7aaeb0150d7d2e33768a5ecc59
Claire Walker <avid.gamer81@gmail.com>
2020-11-23 15:11:55 +0000

    adding all basic functionality

--- app.js
+++ app.js
@@ -3,41 +3,45 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = [];
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // set up to ensure that our server knows we are using EJS as our templating module
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
     
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = "";
 
-    switch (currentDay) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-     day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
-}
+// These are the options for toLocaleDateString
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
 
-    res.render('list', {kindOfDay: day});
+let day = today.toLocaleDateString("en-UK", options);
+
+
+
+res.render('list', {kindOfDay: day, newListItems: items});
 
 });
+
+/* This section of code grabs the added item from the post request, then
+pushes it to the array items. Once that is done, it redirects the page to
+the home route, which then uses the res.render to display the updated page
+with the new item added. See list.ejs for how the items array is looped
+through for each list item. */
+app.post("/", (req, res) => {
+    let item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
+});
+
+
 
 
 
